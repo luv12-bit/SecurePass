8ab11981679b71eafa0439994a1e74d6c7aa0943
luv12-bit <luvmaken107@gmail.com>
2026-04-28 04:10:38 +0530

    backend ready

--- server/server.js
+++ server/server.js
@@ -50,5 +50,5 @@ app.use(require('./middleware/error'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
